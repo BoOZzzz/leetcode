2fea883b677b0efb129f508956fb1f2d4f2ff99b
Zeyeebra <89395205+BoOZzzz@users.noreply.github.com>
2025-08-19 22:03:59 -0400

    Time: 47 ms (53.48%) | Memory: 18.4 MB (66.52%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -5,17 +5,30 @@ class Solution(object):
         :rtype: int
         """
         
-        subarrays = defaultdict(int)
-        arr_size = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                arr_size += 1
-            if nums[i] != 0 and arr_size != 0:
-                subarrays[arr_size] += 1
-                arr_size = 0
-        if arr_size != 0:
-            subarrays[arr_size] += 1
-        sum_arrs = 0
-        for i, v in subarrays.items():
-            sum_arrs += v*((i)*(i+1))/2
-        return sum_arrs
+        # subarrays = defaultdict(int)
+        # arr_size = 0
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         arr_size += 1
+        #     if nums[i] != 0 and arr_size != 0:
+        #         subarrays[arr_size] += 1
+        #         arr_size = 0
+        # if arr_size != 0:
+        #     subarrays[arr_size] += 1
+        # sum_arrs = 0
+        # for i, v in subarrays.items():
+        #     sum_arrs += v*((i)*(i+1))/2
+        # return sum_arrs
+
+        ans = 0
+        cur = 0
+
+        for num in nums:
+            if num == 0:
+                cur += 1
+            else:
+                cur = 0
+            
+            ans += cur
+
+        return ans
